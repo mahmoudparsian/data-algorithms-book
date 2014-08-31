a798ce25063d6bc831a7424de85ca320dbfe7837
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2014-08-30 19:34:19 -0700

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,14 @@
-# set jdk7
+# This is an example to set 3 environment variables
+# You should update your script accrodingly
+#
+#set Java as jdk7
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home
 echo "JAVA_HOME=$JAVA_HOME"
+#
 # set ant 
 export ANT_HOME=/Users/mahmoud/apache-ant-1.9.4
 echo "ANT_HOME=$ANT_HOME"
+#
 # set PATH
 export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$PATH
+echo "PATH=$PATH"
