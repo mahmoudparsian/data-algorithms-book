2f6e5991d85379143cc69e4b9f7f2a682f894bf2
Mahmoud Parsian <mparsian@yahoo.com>
2014-10-07 10:38:10 -0700

    updated package documentation

--- src/main/java/org/dataalgorithms/chap29/combinesmallfilesbyhadoop/package-info.java
+++ src/main/java/org/dataalgorithms/chap29/combinesmallfilesbyhadoop/package-info.java
@@ -3,7 +3,13 @@
  * chapter 29 of the Data Algorithms book published by O'Reilly.
  *
  * This package combines small files using Hadoop's CombineFileInputFormat class.
- *
+ * 
+ * I adopted the solution for this package from the following sources:
+ * 
+ *   1. https://svn.apache.org/repos/asf/hadoop/common/trunk/hadoop-mapreduce-project/hadoop-mapreduce-examples/src/main/java/org/apache/hadoop/examples/MultiFileWordCount.java
+ *   2. http://www.idryman.org/blog/2013/09/22/process-small-files-on-hadoop-using-combinefileinputformat-1/
+ *   3. https://github.com/lintool/Cloud9/blob/master/src/dist/edu/umd/cloud9/io/pair/PairOfStringLong.java
+ *   4. http://blog.yetitrails.com/2011/04/dealing-with-lots-of-small-files-in.html
  *
  * @author Mahmoud Parsian
  *
