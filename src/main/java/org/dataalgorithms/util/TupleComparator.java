1a599cc06eaea5120426386d31af84f75879183e
Mahmoud Parsian <mparsian@illumina.com>
2014-10-15 12:51:53 -0700

    added java.io.Serializable

--- src/main/java/org/dataalgorithms/util/TupleComparator.java
+++ src/main/java/org/dataalgorithms/util/TupleComparator.java
@@ -2,16 +2,17 @@ package org.dataalgorithms.util;
 
 import scala.Tuple2;
 import java.util.Comparator;
- 
+import java.io.Serializable;
+
 /**
  * This class enables to compare two Tuple2<Integer, Integer> objects.
- * Used for sorting purposes. It just compares the first object 
- * from a given two Tuple2.
+ * Used for sorting purposes. It just compares the Tuple2's first elements. 
  *
  * @author Mahmoud Parsian
  *
  */
-public class TupleComparator implements Comparator<Tuple2<Integer, Integer>> {
+public class TupleComparator 
+   implements Comparator<Tuple2<Integer, Integer>>, Serializable {
    public int compare(Tuple2<Integer, Integer> t1, Tuple2<Integer, Integer> t2) {
       return t1._1.compareTo(t2._1);
    }
