d51bce743bd7d9d23a8543db842ebc296d496685
Mahmoud Parsian <mparsian@yahoo.com>
2014-12-12 16:54:06 -0800

    added convenient method to create JavaSparkContext

--- src/main/java/org/dataalgorithms/util/SparkUtil.java
+++ src/main/java/org/dataalgorithms/util/SparkUtil.java
@@ -22,53 +22,49 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class SparkUtil {
 
    /**
-    * Create a JavaSparkContext object from a given YARN's resource manager host
-    *
-    * @param yarnResourceManagerHost the YARN's resource manager host
-    * @return a JavaSparkContext
-    *
-    */
-   public static JavaSparkContext createJavaSparkContext(String yarnResourceManagerHost) 
-      throws Exception {
-      SparkConf conf = new SparkConf();
-      conf.set("yarn.resourcemanager.hostname", yarnResourceManagerHost);
-      conf.set("yarn.resourcemanager.scheduler.address", yarnResourceManagerHost + ":8030");
-      conf.set("yarn.resourcemanager.resource-tracker.address", yarnResourceManagerHost + ":8031");
-      conf.set("yarn.resourcemanager.address",  yarnResourceManagerHost + ":8032");
-      conf.set("mapreduce.framework.name", "yarn");
-      conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-      conf.set("spark.executor.memory", "1g");
-      JavaSparkContext ctx = new JavaSparkContext("yarn-cluster", "spark-program", conf);
-      return ctx;
-   }
-
-   /**
-    * Create a JavaSparkContext object from a given Spark's master URL
-    *
-    * @param sparkMasterURL Spark master URL as "spark://<spark-master-host-name>:7077"
-    * @param description program description
-    * @return a JavaSparkContext
-    *
-    */
-   public static JavaSparkContext createJavaSparkContext(String sparkMasterURL, String description) 
-      throws Exception {
-      JavaSparkContext ctx = new JavaSparkContext(sparkMasterURL, description);   
-      return ctx;
-   }
-   
-   /**
     * Create a JavaSparkContext that loads settings from system properties 
     * (for instance, when launching with ./bin/spark-submit).
     *
     * @return a JavaSparkContext
     *
     */
-   public static JavaSparkContext createJavaSparkContext() 
+    public static JavaSparkContext createJavaSparkContext()  
       throws Exception {
       return new JavaSparkContext();   
    }
+
+   /**
+    * Create a JavaSparkContext object from a given Spark's master URL
+    *
+    * @param sparkMasterURL Spark master URL as "spark://<spark-master-host-name>:7077"
+    * @param applicationName application name
+    * @return a JavaSparkContext
+    *
+    */
+   public static JavaSparkContext createJavaSparkContext(String sparkMasterURL, String applicationName) 
+      throws Exception {
+      JavaSparkContext ctx = new JavaSparkContext(sparkMasterURL, applicationName);   
+      return ctx;
+   }
+   
+    
+   
+   /**
+    * Create a JavaSparkContext that loads settings from system properties 
+    * (for instance, when launching with ./bin/spark-submit).
+    * @param applicationName application name
+    *
+    * @return a JavaSparkContext
+    *
+    */
+    public static JavaSparkContext createJavaSparkContext(String applicationName)  
+      throws Exception { 
+      SparkConf conf = new SparkConf().setAppName(applicationName);
+      JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+      return ctx;
+   }
    
    public static String version() {
-      return "1.0.0";
+      return "1.1.0";
    }   
 }
