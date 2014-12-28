5cec12ffc517c682392a6a2c59eff60ad4f48e5e
Mahmoud Parsian <mparsian@yahoo.com>
2014-12-28 11:26:53 -0800

    improved docs

--- src/main/java/org/dataalgorithms/chap16/spark/package-info.java
+++ src/main/java/org/dataalgorithms/chap16/spark/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package (org.dataalgorithms.chap16.spark) contains source code for 
+ * This package (org.dataalgorithms.chap16.spark) contains source code 
  * for chapter 16 of the Data Algorithms book published by O'Reilly.
  *
  * @author Mahmoud Parsian
