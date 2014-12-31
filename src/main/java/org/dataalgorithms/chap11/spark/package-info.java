418bb8870ebfa3cc84d48df909213cba63a005e7
Mahmoud Parsian <mparsian@yahoo.com>
2014-12-31 11:54:43 -0800

    fixed package name

--- src/main/java/org/dataalgorithms/chap11/spark/package-info.java
+++ src/main/java/org/dataalgorithms/chap11/spark/package-info.java
@@ -1,10 +1,10 @@
 /**
- * This package (org.dataalgorithms.chap11.projection.spark) contains source code
+ * This package (org.dataalgorithms.chap11.spark) contains source code
  * for chapter 11 of the Data Algorithms book published by O'Reilly.
  *
  * @author Mahmoud Parsian
  *
  */
-package org.dataalgorithms.chap11.projection.spark;
+package org.dataalgorithms.chap11.spark;
 
 //rest of the file is empty
