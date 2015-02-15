f1b04b96cabea7ae97ab10bf5d9755bfd4f67bea
Mahmoud Parsian <mparsian@illumina.com>
2015-02-14 19:02:39 -0800

    improved docs

--- src/main/java/org/dataalgorithms/bonus/wordcount/spark/SparkWordCount.java
+++ src/main/java/org/dataalgorithms/bonus/wordcount/spark/SparkWordCount.java
@@ -23,69 +23,72 @@ import java.util.Collections;
  */
 public class SparkWordCount {
 
-  public static void main(String[] args) throws Exception {
-    if (args.length != 3) {
-       System.err.println("Usage: SparkWordCount <N> <input> <output>");
-       System.exit(1);
-    }
-
-	final int N = Integer.parseInt(args[0]);
-    final String inputPath = args[1];
-    final String outputPath = args[2];
-
-	// create a context object, which is used 
-	// as a factory for creating new RDDs
-    JavaSparkContext ctx = new JavaSparkContext();
-
-    JavaRDD<String> lines = ctx.textFile(inputPath, 1);
-
-    //      output                                            input   output
-    JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-       //              output       input
-       public Iterable<String> call(String s) {
-		  if ((s == null) || (s.length() < N)) {
-	  		 return Collections.emptyList();
-	  	  }
-       
-          String[] tokens = s.split(" ");
-          List<String> list = new ArrayList<String>();
-          for (String  tok : tokens) {
-        	
-	  	    if (tok.matches(".*[,.;]$")) {
-	  	        // remove the special char from the end
-	  	    	tok = tok.substring(0, tok.length() -1); 
-	  	    }
-        	
-        	if (tok.length() < N) {
-        		continue;
-        	}
-        	
-        	list.add(tok);
-         }
-         return list;
+    public static void main(String[] args) throws Exception {
+       if (args.length != 3) {
+          System.err.println("Usage: SparkWordCount <N> <input> <output>");
+          System.exit(1);
        }
-    });
 
-    //          K       V                                                input   K       V
-    JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
-      //            K       V             input
-      public Tuple2<String, Integer> call(String s) {
-        //                K       V
-        return new Tuple2<String, Integer>(s, 1);
-      }
-    });
+       // handle input parameters
+       final int N = Integer.parseInt(args[0]);
+       final String inputPath = args[1];
+       final String outputPath = args[2];
 
-    JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
-      public Integer call(Integer i1, Integer i2) {
-        return i1 + i2;
-      }
-    });
+       // create a context object, which is used 
+       // as a factory for creating new RDDs
+       JavaSparkContext ctx = new JavaSparkContext();
 
-    // save the final output 
-    counts.saveAsTextFile(outputPath);
+       // read input and create the first RDD
+       JavaRDD<String> lines = ctx.textFile(inputPath, 1);
 
-	// close the context and we are done
-    ctx.close();
-    System.exit(0);
-  }
+       //      output                                            input   output
+       JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+          //              output       input
+          public Iterable<String> call(String s) {
+             if ((s == null) || (s.length() < N)) {
+                return Collections.emptyList();
+             }
+             
+             String[] tokens = s.split(" ");
+             List<String> list = new ArrayList<String>();
+             for (String  tok : tokens) {
+                if (tok.matches(".*[,.;]$")) {
+                   // remove the special char from the end
+                   tok = tok.substring(0, tok.length() -1); 
+                }
+           
+                if (tok.length() < N) {
+                   continue;
+                }
+           
+                list.add(tok);
+             }
+             return list;
+          }
+       });
+
+       //          K       V                                                input   K       V
+       JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
+         //            K       V             input
+         public Tuple2<String, Integer> call(String s) {
+           //                K       V
+           return new Tuple2<String, Integer>(s, 1);
+         }
+       });
+
+       // find the total count for each unique word
+       JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+          public Integer call(Integer i1, Integer i2) {
+             return i1 + i2;
+          }
+       });
+
+       // save the final output 
+       counts.saveAsTextFile(outputPath);
+
+       // close the context and we are done
+       ctx.close();
+       System.exit(0);
+    }
+    
 }
