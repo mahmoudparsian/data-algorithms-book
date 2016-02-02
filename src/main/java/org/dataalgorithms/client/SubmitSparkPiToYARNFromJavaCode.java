dc3f8778c3fd9cf0a408e06403d58a975b882a58
Mahmoud Parsian <mparsian@illumina.com>
2016-02-02 12:36:57 -0800

    submit SparkPi to YARN from Java code

---
+++ src/main/java/org/dataalgorithms/client/SubmitSparkPiToYARNFromJavaCode.java
@@ -0,0 +1,87 @@
+package org.dataalgorithms.client;
+
+import java.util.List;
+import java.util.Arrays;
+import org.apache.spark.SparkConf;
+import org.apache.spark.deploy.yarn.Client;
+import org.apache.spark.deploy.yarn.ClientArguments;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.log4j.Logger;
+
+/**
+ * This class submits a SparkPi to a YARN from a Java client (as opposed 
+ * to submitting a Spark job from a shell command line using spark-submit).
+ * 
+ * To accomplish submitting a Spark job from a Java client, we use 
+ * the org.apache.spark.deploy.yarn.Client class described below:
+ * 
+ Usage: org.apache.spark.deploy.yarn.Client [options]
+ Options:
+  --jar JAR_PATH           Path to your application's JAR file (required in yarn-cluster mode)
+  --class CLASS_NAME       Name of your application's main class (required)
+  --primary-py-file        A main Python file
+  --arg ARG                Argument to be passed to your application's main class. 
+                           Multiple invocations are possible, each will be passed in order.
+  --num-executors NUM      Number of executors to start (Default: 2)
+  --executor-cores NUM     Number of cores per executor (Default: 1).
+  --driver-memory MEM      Memory for driver (e.g. 1000M, 2G) (Default: 512 Mb)
+  --driver-cores NUM       Number of cores used by the driver (Default: 1).
+  --executor-memory MEM    Memory per executor (e.g. 1000M, 2G) (Default: 1G)
+  --name NAME              The name of your application (Default: Spark)
+  --queue QUEUE            The hadoop queue to use for allocation requests (Default: 'default')
+  --addJars jars           Comma separated list of local jars that want SparkContext.addJar to work with.
+  --py-files PY_FILES      Comma-separated list of .zip, .egg, or .py files to place on the PYTHONPATH for Python apps.
+  --files files            Comma separated list of files to be distributed with the job.
+  --archives archives      Comma separated list of archives to be distributed with the job.
+
+* 
+*  @author Mahmoud Parsian (mahmoud.parsian@yahoo.com)
+* 
+*/
+
+public class SubmitSparkPiToYARNFromJavaCode {
+    
+    static final Logger THE_LOGGER = Logger.getLogger(SubmitSparkPiToYARNFromJavaCode.class);
+    
+    public static void main(String[] arguments) throws Exception {
+        long startTime = System.currentTimeMillis();    
+        test(arguments); // ... the code being measured ...    
+        long elapsedTime = System.currentTimeMillis() - startTime;   
+        THE_LOGGER.info("elapsedTime (millis)="+elapsedTime);
+    }        
+    
+    static void test(String[] arguments) throws Exception {           
+        String[] args = new String[]{
+            "--name",
+            "test-SparkPi",
+            //
+            "--driver-memory",
+            "1000M",
+            //
+            "--jar",
+            "/Users/mparsian/spark-1.6.0/examples/target/scala-2.10/spark-examples-1.6.0-hadoop2.5.0.jar",
+            //
+            "--class",
+            "org.apache.spark.examples.SparkPi",
+            
+            // argument 1 to my Spark program
+            "--arg",
+            "3",            
+            
+            // argument 2 to my Spark program (helper argument to create a proper JavaSparkContext object)
+            "--arg",
+            "yarn-cluster"
+        };
+
+        Configuration config = new Configuration();
+        //
+        System.setProperty("SPARK_YARN_MODE", "true");
+        //
+        SparkConf sparkConf = new SparkConf();
+        ClientArguments clientArgs = new ClientArguments(args, sparkConf);
+        Client client = new Client(clientArgs, config, sparkConf);
+        //
+        client.run(); 
+        // done!
+    }
+}
