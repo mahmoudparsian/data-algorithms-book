cb43b8e126aa14bc4ff82078eca3408ae8cc9d59
Mahmoud Parsian <mparsian@illumina.com>
2016-06-28 23:00:32 -0700

    improved client package documentation...

---
+++ src/main/java/org/dataalgorithms/client/package-info.java
@@ -0,0 +1,12 @@
+package org.dataalgorithms.client;
+/**
+ * This package (org.dataalgorithms.client) contains 
+ * source code for understanding Spark clients.
+ *
+
+ *
+ * @author Mahmoud Parsian
+ *
+ */
+
+//rest of the file is empty
---
+++ src/main/java/org/dataalgorithms/machinelearning/package-info.java
@@ -0,0 +1,12 @@
+package org.dataalgorithms.machinelearning;
+/**
+ * This package (org.dataalgorithms.machinelearning) contains 
+ * source code and examples for understanding Spark's MLlib.
+ *
+
+ *
+ * @author Mahmoud Parsian
+ *
+ */
+
+//rest of the file is empty
