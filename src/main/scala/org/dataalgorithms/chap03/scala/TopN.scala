73caab39d65f71fc9bc0e93ee453455ef2997b0c
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2016-07-25 22:19:11 -0700

    added scala version of Top-N design pattern...

---
+++ src/main/scala/org/dataalgorithms/chap03/scala/TopN.scala
@@ -0,0 +1,61 @@
+package org.dataalgorithms.chap03.scala;
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import scala.collection.SortedMap
+
+/**
+ * Find TopN (N > 0) using mapPartitions().
+ * Each partition finds TopN, then we find TopN of all partitions.
+ *           
+ *
+ * @author Gaurav Bhardwaj (gauravbhardwajemail@gmail.com)
+ * 
+ * @editor Mahmoud Parsian (mahmoud.parsian@yahoo.com)
+ *
+ */
+object TopN {
+
+  def main(args: Array[String]): Unit = {
+    val config = new SparkConf
+    config.setAppName("TopN")
+    config.setMaster("local[*]")
+    val sc = new SparkContext(config)
+
+    val N = sc.broadcast(10)
+    val path = "/path/to/the/folder"
+
+    val input = sc.textFile(path)
+    val pair = input.map(line => {
+      val tokens = line.split(",")
+      (tokens(2).toInt, tokens)
+    })
+
+    import Ordering.Implicits._
+    val partitions = pair.mapPartitions(itr => {
+      var sortedMap = SortedMap.empty[Int, Array[String]]
+      itr.foreach { tuple =>
+        {
+          sortedMap += tuple
+          if (sortedMap.size > N.value) {
+            sortedMap = sortedMap.takeRight(N.value)
+          }
+        }
+      }
+      sortedMap.takeRight(N.value).toIterator
+    })
+  
+    val alltop10 = partitions.collect()
+    val finaltop10 = SortedMap.empty[Int, Array[String]].++:(alltop10)
+    val yourResult = finaltop10.takeRight(N.value)
+    yourResult.foreach { 
+      case (k, v) => println(s"$k \t ${v.asInstanceOf[Array[String]].mkString(",")}") 
+    }
+
+    val result = pair.groupByKey().sortByKey(false).take(N.value)
+    result.foreach { case (k, v) => println(s"$k \t ${v.flatten.mkString(",")}") }
+
+    // done
+    sc.stop()
+  }
+}
---
+++ src/main/scala/org/dataalgorithms/chap03/scala/TopNNonUnique.scala
@@ -0,0 +1,66 @@
+package org.dataalgorithms.chap03.scala;
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import scala.collection.SortedMap
+
+/**
+ * Find TopN (N > 0) using mapPartitions().
+ * Each partition finds TopN, then we find TopN of all partitions.
+ *           
+ *
+ * @author Gaurav Bhardwaj (gauravbhardwajemail@gmail.com)
+ * 
+ * @editor Mahmoud Parsian (mahmoud.parsian@yahoo.com)
+ *
+ */
+object TopNNonUnique {
+  
+  def main(args: Array[String]): Unit = {
+    val config = new SparkConf
+    config.setAppName("TopNNonUnique")
+    config.setMaster("local[*]")
+    val sc = new SparkContext(config)
+
+    val N = sc.broadcast(2)
+    val path = "/path/to/the/folder"
+
+    val input = sc.textFile(path)
+    val kv = input.map(line => {
+      val tokens = line.split(",")
+      (tokens(0), tokens(1).toInt)
+    })
+    
+    val uniqueKeys = kv.reduceByKey(_ + _)
+    import Ordering.Implicits._
+    val partitions = uniqueKeys.mapPartitions(itr => {
+      var sortedMap = SortedMap.empty[Int, String]
+      itr.foreach { tuple => {
+        sortedMap += tuple.swap
+        if (sortedMap.size > N.value) {
+          sortedMap = sortedMap.takeRight(N.value)
+        }
+      }}
+      sortedMap.takeRight(N.value).toIterator
+    })
+  
+    val alltop10 = partitions.collect()
+    val finaltop10 = SortedMap.empty[Int, String].++:(alltop10)
+    val yourResult = finaltop10.takeRight(N.value)
+    yourResult.foreach { case (k, v) => println(s"$k \t ${v.mkString(",")}") }
+
+    val createCombiner = (v: Int) => v
+    val mergeValue = (a: Int, b: Int) => (a + b)
+    val result = kv.combineByKey(createCombiner, mergeValue, mergeValue)
+                   .map(_.swap)
+                   .groupByKey()
+                   .sortByKey(false).take(N.value)
+    //              
+    result.foreach { 
+      case (k, v) => println(s"$k \t ${v.mkString(",")}") 
+    }
+
+    // done
+    sc.stop()
+  }
+}
