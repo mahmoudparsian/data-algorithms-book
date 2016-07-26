4594ca8c28b1e910d8fb1ebbb1d1a7f1b362d7ca
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2016-07-25 18:04:49 -0700

    scala solution for secondary sort

---
+++ src/main/scala/org/dataalgorithms/chap01/scala/CustomPartitioner.scala
@@ -0,0 +1,38 @@
+package org.dataalgorithms.chap01.scala
+
+import org.apache.spark.Partitioner
+
+
+/**
+ * A custom partitioner
+ * 
+ * org.apache.spark.Partitioner:
+ *           An object that defines how the elements in a key-value 
+ *           pair RDD are partitioned by key. Maps each key to a 
+ *           partition ID, from 0 to numPartitions - 1.
+ *           
+ *
+ * @author Gaurav Bhardwaj (gauravbhardwajemail@gmail.com)
+ * 
+ * @editor Mahmoud Parsian (mahmoud.parsian@yahoo.com)
+ *
+ */
+class CustomPartitioner(partitions: Int) extends Partitioner {
+  
+  require(partitions > 0, s"Number of partitions ($partitions) cannot be negative.")
+
+  def numPartitions: Int = partitions
+
+  def getPartition(key: Any): Int = key match {
+    case (k: String, v: Int) => math.abs(k.hashCode % numPartitions)
+    case null                => 0
+    case _                   => math.abs(key.hashCode % numPartitions)
+  }
+
+  override def equals(other: Any): Boolean = other match {
+    case h: CustomPartitioner => h.numPartitions == numPartitions
+    case _                    => false
+  }
+
+  override def hashCode: Int = numPartitions
+}
---
+++ src/main/scala/org/dataalgorithms/chap01/scala/SecondarySort.scala
@@ -0,0 +1,54 @@
+package org.dataalgorithms.chap01.scala
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+/**
+ * Spark/Scala solution to secondary sort
+ *
+ * @author Gaurav Bhardwaj (gauravbhardwajemail@gmail.com)
+ * 
+ * @editor Mahmoud Parsian (mahmoud.parsian@yahoo.com)
+ *
+ */
+object SecondarySort {
+  
+  def main(args: Array[String]): Unit = {
+    if (args.length != 3) {
+      println("Usage <No of Partition> <input dir> <output dir>")
+      sys.exit(1)
+    }
+
+    val partitions = args(0).toInt
+    val inputPath = args(1)
+    val outputPath = args(2)
+
+    val config = new SparkConf
+    config.setAppName("SecondarySort")
+    val sc = new SparkContext(config)
+
+    val input = sc.textFile(inputPath)
+
+    val valueToKey = input.map(x => {
+      val line = x.split(",")
+      ((line(0) + "-" + line(1), line(3).toInt), line(3).toInt)
+    })
+
+    implicit def tupleOrderingDesc = new Ordering[Tuple2[String, Int]] {
+      override def compare(x: Tuple2[String, Int], y: Tuple2[String, Int]): Int = {
+        if (y._1.compare(x._1) == 0) y._2.compare(x._2)
+        else y._1.compare(x._1)
+      }
+    }
+
+    val sorted = valueToKey.repartitionAndSortWithinPartitions(new CustomPartitioner(partitions))
+
+    val result = sorted.map {
+      case (k, v) => (k._1, v)
+    }
+
+    result.saveAsTextFile(outputPath)
+
+    sc.stop
+  }
+}
