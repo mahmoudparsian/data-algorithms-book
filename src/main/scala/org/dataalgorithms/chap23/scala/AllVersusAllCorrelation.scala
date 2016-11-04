b0915c842b16b07a286537a75936e5ba672effbb
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2016-11-04 11:09:34 -0700

    all vs. all correlation added using scala/spark

---
+++ src/main/scala/org/dataalgorithms/chap23/scala/AllVersusAllCorrelation.scala
@@ -0,0 +1,100 @@
+package org.dataalgorithms.chap23.scala
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+/**
+ *
+ * Correlate "all-samples" vs. "all-samples".
+ * Here we use Apache's PearsonsCorrelation, 
+ * but you may replace it with your desired 
+ * correlation algorithm.
+ * 
+ * @author Gaurav Bhardwaj (gauravbhardwajemail@gmail.com)
+ *
+ * @editor Mahmoud Parsian (mahmoud.parsian@yahoo.com)
+ *
+ */
+object AllVersusAllCorrelation {
+
+  def main(args: Array[String]): Unit = {
+    //
+    if (args.size < 3) {
+      println("Usage: AllVersusAllCorrelation <input-sample-reference> <input-dir-bioset> <output-dir>")
+      //
+      // <input-sample-reference> can be one of the following:
+      //      "r1" = normal sample
+      //      "r2" = disease sample
+      //      "r3" = paired sample
+      //      "r4" = unknown
+      //      
+      sys.exit(1)
+    }
+
+    val sparkConf = new SparkConf().setAppName("AllVersusAllCorrelation")
+    val sc = new SparkContext(sparkConf)
+
+    val inputReference = args(0) // {"r1", "r2", "r3", "r4"}
+    val inputBioset = args(1)
+    val output = args(2)
+
+    val ref = sc.broadcast(inputReference)
+    val biosets = sc.textFile(inputBioset)
+
+    // keep the sample matching with the <input-sample-reference>
+    val filtered = biosets.filter(_.split(",")(1).equals(ref.value))
+
+    val pairs = filtered.map(record => {
+      val tokens = record.split(",")
+      (tokens(0), (tokens(2), tokens(3).toDouble))
+    })
+
+    // note that groupByKey() provides an expensive solution 
+    // [you must have enough memory/RAM to hold all values for 
+    // a given key -- otherwise you might get OOM error], but
+    // combineByKey() and reduceByKey() will give a better 
+    // scale-out performance 
+    val grouped = pairs.groupByKey()
+
+    val cart = grouped.cartesian(grouped)
+
+    val filtered2 = cart.filter(pair => pair._1._1 < pair._2._1)
+
+    val finalresult = filtered2.map(t => {
+      val g1 = t._1
+      val g2 = t._2
+      val g1Map = g1._2.toMap.groupBy(_._1).mapValues(itr => (itr.values.sum, itr.values.size))
+      val g2Map = g2._2.toMap.groupBy(_._1).mapValues(itr => (itr.values.sum, itr.values.size))
+      val xy = for {
+        g1Entry <- g1Map
+        g1PatientID = g1Entry._1
+        g2MD = g2Map.get(g1PatientID)
+        if (g2MD.isDefined)
+      } yield ((g1Entry._2._1 / g1Entry._2._2, g2MD.get._1 / g2MD.get._2))
+
+      val (x, y) = xy.unzip
+      val k = (g1._1, g2._1)
+      if (x.size < 3) (k, (Double.NaN, Double.NaN))
+      else {
+        import org.apache.commons.math3.distribution.TDistribution
+        import org.apache.commons.math3.stat.correlation.PearsonsCorrelation
+        val PC = new PearsonsCorrelation()
+        val correlation = PC.correlation(x.toArray, y.toArray)
+        val t = math.abs(correlation * math.sqrt((x.size - 2.0) / (1.0 - (correlation * correlation))))
+        val tdist = new TDistribution(x.size - 2)
+        val pvalue = 2 * (1.0 - tdist.cumulativeProbability(t))
+        (k, (correlation, pvalue))
+      }
+    })
+
+    // For deugging purpose
+    finalresult.foreach(println)
+
+    // save final output
+    finalresult.saveAsTextFile(output)
+
+    // done!
+    sc.stop()
+  }
+
+}
---
+++ src/main/scala/org/dataalgorithms/chap23/scala/README.md
@@ -0,0 +1,35 @@
+[![All vs. All Correlation](./correlation.gif)]()
+
+Correlation
+===========
+A correlation is a single number that describes the 
+degree of relationship between two variables (samples 
+of N-dimensions). Correlation is a measure of association 
+of samples while "regression" is the fitting of a function 
+to a set of observations.
+
+
+In statistics, the Pearson product-moment correlation 
+coefficient is a measure of the linear dependence between 
+two variables X and Y, giving a value between -1.0 and +1.0 
+inclusive, where +1.0 is total positive linear correlation, 
+0.0 is no linear correlation, and −1.0 is total negative 
+linear correlation. It is widely used in the sciences.
+
+
+Possible correlations range from -1.0 to +1.0. 
+* A zero correlation indicates that there is no relationship 
+  between the variables. 
+* A correlation of –1.0 indicates a perfect negative correlation, 
+  meaning that as one variable goes up, the other goes down. 
+* A correlation of +1.0 indicates a perfect positive correlation, 
+  meaning that both variables move in the same direction together.
+
+Sources and References
+======================
+
+* [Pearson product-moment correlation coefficient](https://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient)
+
+* [Correlation and dependence](https://en.wikipedia.org/wiki/Correlation_and_dependence)
+
+* [What Is Correlation?](https://www.verywell.com/what-is-correlation-2794986)
