1557ffb816ae8f46e50f72a26337d2c4cf409954
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2017-01-13 09:51:49 -0800

    clean up

--- src/main/java/org/dataalgorithms/chap04/mapreduce/LocationCountReducer.java
+++ src/main/java/org/dataalgorithms/chap04/mapreduce/LocationCountReducer.java
@@ -1,11 +1,13 @@
 package org.dataalgorithms.chap04.mapreduce;
 
+import java.io.IOException;
+//
+import java.util.Set;
+import java.util.HashSet;
+//
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
-import java.io.IOException;
-import java.util.Set;
-import java.util.HashSet;
 
 /** 
  * LocationCountReducer implements the reduce() function for counting locations.
@@ -14,18 +16,18 @@ import java.util.HashSet;
  *
  */
 public  class LocationCountReducer 
-	extends Reducer<Text, Text, Text, LongWritable> {
-	
+    extends Reducer<Text, Text, Text, LongWritable> {
+
     @Override
     public void reduce(Text productID, Iterable<Text> locations, Context context)
         throws  IOException, InterruptedException {
-		
-		Set<String> set = new HashSet<String>();
-
+        //
+        Set<String> set = new HashSet<String>();
+        //
         for (Text location: locations) {
            set.add(location.toString());
         }
-
+        //
         context.write(productID, new LongWritable(set.size()));
     }
 }
