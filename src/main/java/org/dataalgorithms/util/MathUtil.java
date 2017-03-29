59f49ef25ca471d4775b081701e7ef476fe6c2e1
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2017-03-29 09:01:19 -0700

    removed dead stores...

--- src/main/java/org/dataalgorithms/util/MathUtil.java
+++ src/main/java/org/dataalgorithms/util/MathUtil.java
@@ -6,8 +6,8 @@ import org.apache.commons.math.stat.inference.TTest;
 import org.apache.commons.math.stat.inference.TTestImpl;
 
 /**
- * MathUtil is a utility class, which provides some basic math. 
- * and statistical functions (such as ttest).
+ * MathUtil is a utility class, which provides some basic 
+ * math. and statistical functions (such as ttest).
  *
  * @author Mahmoud Parsian
  *
@@ -22,23 +22,21 @@ public class MathUtil {
             return Double.NaN;
         }
 
-        double score;
         try {
             if (arrA.length == 1) {
-                score = ttest.tTest(arrA[0], arrB);
+                return ttest.tTest(arrA[0], arrB);
             } 
             else if (arrB.length == 1) {
-                score = ttest.tTest(arrB[0], arrA);
+                return ttest.tTest(arrB[0], arrA);
             } 
             else {
-                score = ttest.tTest(arrA, arrB);
+                return ttest.tTest(arrA, arrB);
             }
         } 
         catch (Exception e) {
             e.printStackTrace();
-            score = Double.NaN;
+            return Double.NaN;
         }
-        return score;
     }
 
     public static double ttest(List<Double> groupA, List<Double> groupB) {
@@ -46,17 +44,15 @@ public class MathUtil {
             return Double.NaN;
         }
 
-        double score;
         if (groupA.size() == 1) {
-            score = tTest(groupA.get(0), groupB);
+            return tTest(groupA.get(0), groupB);
         } 
         else if (groupB.size() == 1) {
-            score = tTest(groupB.get(0), groupA);
+            return tTest(groupB.get(0), groupA);
         } 
         else {
-            score = tTest(groupA, groupB);
+            return tTest(groupA, groupB);
         }
-        return score;
     }
 
     private static double tTest(double d, List<Double> group) {
@@ -94,85 +90,4 @@ public class MathUtil {
         return arr;
     }
 
-<<<<<<< HEAD
-=======
-	public static double ttest(double[] arrA, double[] arrB) {
-		if ((arrA.length == 1) && (arrB.length == 1)) {
-			// return a NULL value for score (does not make sense)
-			return Double.NaN;
-		}
-	
-		double score;
-		try {
-			if (arrA.length == 1) {
-				score = ttest.tTest(arrA[0], arrB);
-			} 
-			else if (arrB.length == 1) {
-				score = ttest.tTest(arrB[0], arrA);
-			} 
-			else {
-				score = ttest.tTest(arrA, arrB);
-			}
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			score = Double.NaN;
-		}						
-		return score;
-	}
-	
-	public static double ttest(List<Double> groupA, List<Double> groupB) {
-		if ((groupA.size() == 1) && (groupB.size() == 1)) {
-			return Double.NaN;
-		}
-	
-		double score;
-		if (groupA.size() == 1) {
-			score = tTest(groupA.get(0), groupB);
-		} 
-		else if (groupB.size() == 1) {
-			score = tTest(groupB.get(0), groupA);
-		} 
-		else {
-			score = tTest(groupA, groupB);
-		}			
-		return score;
-	}
-	
-	private static double tTest(double d, List<Double> group) {
-		try {
-			double[] arr = listToArray(group);   
-			return ttest.tTest(d, arr);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			return Double.NaN;
-		}
-	}
-	
-	private static double tTest(List<Double> groupA, List<Double> groupB) {
-		try {
-			double[] arrA = listToArray(groupA);  
-			double[] arrB = listToArray(groupB);  
-			return ttest.tTest(arrA, arrB);  
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			return 0.0d;
-		}
-	}
-	
-	static double[] listToArray(List<Double> list) {
-		if ( (list == null) || (list.isEmpty()) ) {
-			return null;
-		}
-		
-		double[] arr = new double[list.size()];
-		for (int i=0; i < arr.length; i++) {
-			arr[i] = list.get(i);
-		}
-		return arr;
-	}
-			
->>>>>>> 960fda52aea18438f95ddb5deff1028ff42ac007
 }
